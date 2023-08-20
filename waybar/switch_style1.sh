22b55cdd9669f5caed6e87d421502fe27dc5e065
5nik7 <nickflip86@gmail.com>
2023-08-20 15:03:33 -0700

    sawqe

--- waybar/switch_style1.sh
+++ waybar/switch_style1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 killall waybar
 SDIR="$HOME/.config/waybar"
-waybar -c "$SDIR"/config1 -s "$SDIR"/style1.css &
+waybar -c "$SDIR"/config -s "$SDIR"/style.css &
 swaymsg gaps outer all set 0 && swaymsg gaps inner all set 0
-
