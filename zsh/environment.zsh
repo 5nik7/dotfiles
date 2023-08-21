a458e851af9c05b8c37ad3778ead22a0acd46a3d
5nik7 <nickflip86@gmail.com>
2023-08-20 18:06:52 -0700

    wow

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -48,36 +48,35 @@ export BAT_THEME='Catppuccin-mocha'
 # FZF bases
 export RG_DEFAULT_COMMAND="rg -i -l --hidden"
 
-export FZF_DEFAULT_COMMAND="rg --files"
+# export FZF_DEFAULT_COMMAND="fd --type f"
 
-export FZF_DEFAULT_OPTS="
-  --color fg:#cdd6f4
-  --color fg+:#fab387 
-  --color bg:#181825
-  --color bg+:#313244
-  --color preview-bg:#11111b
-  --color hl:#f2cdcd
-  --color hl+:#f5e0dc
-  --color info:#cba6f7
-  --color prompt:#f9e2af
-  --color spinner:#f5e0dc
-  --color pointer:#94e2d5
-  --color marker:#f5e0dc
-  --color border:#11111b
-  --color header:#f38ba8
-  --color gutter:#1e1e2e
-  --prompt '> '
-  --pointer ' >'
-  --ansi
-  --layout=reverse
-  --no-scrollbar
-  --info=inline
-  --border=thinblock
-  --margin='2%,2%,2%,2%'
-  --height='95%'
-  --preview='bat -n --style=plain --color=always {}'
-  --preview-window='right'
-  --preview-window='65%'
-  --preview-window='border-thinblock'"
+
+#   --color fg+:#fab387 
+#   --color bg:#181825
+#   --color bg+:#313244
+#   --color preview-bg:#11111b
+#   --color hl:#f2cdcd
+#   --color hl+:#f5e0dc
+#   --color info:#cba6f7
+#   --color prompt:#f9e2af
+#   --color spinner:#f5e0dc
+#   --color pointer:#94e2d5
+#   --color marker:#f5e0dc
+#   --color border:#11111b
+#   --color header:#f38ba8
+#   --color gutter:#1e1e2e
+#   --prompt '> '
+#   --pointer ' >'
+#   --ansi
+#   --layout=reverse
+#   --no-scrollbar
+#   --info=inline
+#   --border=thinblock
+#   --margin='1%,1%,1%,1%'
+#   --height='95%'
+#   --preview='bat -n --style=plain --color=always {}'
+#   --preview-window='right'
+#   --preview-window='65%'
+#   --preview-window='border-thinblock'"
 
 # vim:ft=zsh
