7d9a926b78c2068b82920b8e6304f0d87f756c4e
Nick <91869577+5nik7@users.noreply.github.com>
2023-08-22 17:17:20 -0700

    Update aliases.bash

--- shell/aliases.bash
+++ shell/aliases.bash
@@ -1,5 +1,5 @@
 function cd() {
-	builtin cd "$@" && lsd -la --group-directories-first --blocks permission,name
+    builtin cd "$@" && lsd -la --group-directories-first --blocks permission,name
 }
 
 alias c="clear"
@@ -22,34 +22,31 @@ alias installed="grep -i installed /var/log/pacman.log"
 
 # use exa if available
 if [[ -x "$(command -v lsd)" ]]; then
-	alias ll="lsd -al --group-directories-first"
-	alias l="lsd -la --group-directories-first --blocks permission,name"
+  alias ll="lsd -al --group-directories-first"
+  alias l="lsd -la --group-directories-first --blocks permission,name"
 else
-	alias l="ls -lah ${colorflag}"
-	alias ll="ls -lFh ${colorflag}"
+  alias l="ls -lah ${colorflag}"
+  alias ll="ls -lFh ${colorflag}"
 fi
 
 alias la="ls -AF ${colorflag}"
 alias lld="ls -l | grep ^d"
-alias cp='cp -rv'
-alias mv='mv -v'
 alias srm='sudo rm -rfv'
-alias ln='ln -sfv'
 alias mkdir='mkdir -pv'
 alias path='echo $PATH | tr ":" "\n"'
 alias cat='bat'
-alias mv='mv -vf'
+alias mv='mv -v'
 alias cp='cp -vr'
 alias rm='rm -vrf'
-alias vi="$EDITOR"
 alias e="$EDITOR"
 alias v="$EDITOR"
+alias vi="$EDITOR"
 alias d='ranger'
-alias update='sudo pacman -Syyuu --noconfirm'
+alias update='sudo pacman -Syyu --noconfirm'
 alias dots='cd $DOTFILES'
 alias do75='~/.scripts/do75'
 alias repos='cd /mnt/c/repos'
 
 alias lg="lazygit"
 
-# vim:ft=sh
+# vim:ft=zsh
