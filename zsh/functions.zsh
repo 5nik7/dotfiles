cfbb8c3a36e612e21aef01f67c635e4ecff31560
Nick <91869577+5nik7@users.noreply.github.com>
2023-09-19 17:21:54 -0700

    Update functions.zsh

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,5 @@
 function cd() {
-	builtin cd "$@" && eza -la --icons --hyperlink --git-repos --git --group-directories-first --no-filesize --no-user --no-time --no-permissions
+	builtin cd "$@" && eza -la --icons --hyperlink --git-repos --git --group-directories-first --no-filesize --no-user --no-time
 }
 
 function fixpath() {
